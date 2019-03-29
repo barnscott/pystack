552486edc5c100157387490bc9554624a536d7ed
barnscott <barn.lewis@protonmail.com>
2019-03-28 23:35:00 -0500

    fix os.env location

--- flask/web/database.py
+++ flask/web/database.py
@@ -2,14 +2,12 @@ import psycopg2
 import sys
 import os
 
-
+host = os.environ['POSTGRES_DB']
+dbname = os.environ['POSTGRES_DB']
+user = os.environ['POSTGRES_USER']
+password = os.environ['POSTGRES_PASSWORD']
 
 def connector():
-
-    host = os.environ['POSTGRES_DB']
-    dbname = os.environ['POSTGRES_DB']
-    user = os.environ['POSTGRES_USER']
-    password = os.environ['POSTGRES_PASSWORD']
 
     con = None
     con = psycopg2.connect(host=host, dbname=dbname, user=user, password=password )
